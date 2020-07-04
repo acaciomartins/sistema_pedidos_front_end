d4cba20eb14f33906d77c8d76ad2a92fdf9fbd32
Acácio Amorelli Martins <acacio.am@gmail.com>
2020-07-04 09:10:57 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const multer = require('multer')
 const app = express();
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, '../api/src/assets/produtos/uploads/')
+        cb(null, './src/assets/produtos/uploads/')
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname);
